2703454e87a726ec9a669a6801cbdc619390a278
Ben Boeckel <mathstuf@gmail.com>
2012-03-17 11:55:55 -0400

    Add initial code

---
+++ src/net/benboeckel/tools/media/playlistedit/PlaylistEditActivity.java
@@ -0,0 +1,15 @@
+package net.benboeckel.tools.media.playlistedit;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PlaylistEditActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void
+    onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.main);
+    }
+}
